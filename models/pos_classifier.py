95f709850b81e6a28dbf2113c7c9f06a97eaa05c
Giulio Starace <giulio.starace@gmail.com>
2022-05-11 18:32:48 +0200

    move xneutral logic to pos_classifier model

--- models/pos_classifier.py
+++ models/pos_classifier.py
@@ -1,6 +1,9 @@
 from .base_classifier import BaseClassifier
+import torch
 from torch import nn, Tensor
 from transformers import BatchEncoding
+from torch.nn.utils.rnn import pad_sequence
+import torch.nn.functional as F
 from typing import Tuple
 
 
@@ -21,3 +24,45 @@ class POSClassifier(BaseClassifier):
         encoded_inputs, targets = batch
         logits = self(encoded_inputs)
         return logits, targets
+
+    def validation_step(self, batch, _):
+        encodings, targets = batch
+        batched_embs = self.bert(encodings)
+        batched_preds = self.classifier(batched_embs)
+        batch_size = len(batched_preds)
+
+        acc = self.calculate_average_accuracy(batched_preds, targets)
+
+        # Pad target values to use with CE
+        targets_padded = pad_sequence(targets, batch_first=True, padding_value=-1)
+        loss = F.cross_entropy(
+            batched_preds.permute(0, 2, 1),
+            targets_padded,
+            ignore_index=-1,
+        )
+
+        self.log("val_acc", acc, batch_size=batch_size)
+        self.log("val_loss", loss, batch_size=batch_size)
+
+        # for each sentence in batch
+        for embs, preds, target in zip(batched_embs, batched_preds, targets):
+            preds = preds[: len(target), :].argmax(dim=1).tolist()
+            # for each word in sentence
+            for emb, pred in zip(embs, preds):
+                if pred not in self.tag_centroids:
+                    self.tag_centroids[pred] = [emb]
+                else:
+                    self.tag_centroids[pred].append(emb)
+
+    def on_validation_epoch_start(self):
+        # we overwrite tag_centroids every epoch
+        self.tag_centroids = {}
+
+    def on_validation_epoch_end(self):
+        """
+        at the end of the epoch, we take the mean of vectors
+        accumulated for each tag and store it
+        """
+        self.tag_centroids = {
+            k: torch.mean(torch.vstack(v), dim=0) for k, v in self.tag_centroids.items()
+        }
