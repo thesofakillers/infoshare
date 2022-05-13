d37279df93b3981c6191b688b29fc5ff9bda3204
dinos <papakosk@csd.auth.gr>
2022-05-13 21:38:47 +0200

    Add bash script to collect checkpoints

---
+++ lisa/arrays/get_checkpoints.sh
@@ -0,0 +1,23 @@
+if [ $# -eq 0 ]; then
+    echo "Please specify the task to collect the checkpoints for!"
+    echo "Example: sh get_checkpoints.sh POS"
+    exit 1
+fi
+
+(
+  if [[ $(basename "$(pwd)") == "arrays" ]]; then
+      # Script should be called in the base directory
+      cd ../..
+  fi
+
+  CHECKPOINTS=$(find . -type f -name "*.ckpt" | grep "$1" | sort -V)
+
+  if [[ ${#CHECKPOINTS} -eq 0 ]]; then
+      echo "No checkpoints were found for task $1!"
+  else
+      WRITE_FILE="lisa/arrays/all_checkpoints_${1,,}.txt"
+      echo "Found $(echo "$CHECKPOINTS" | wc -l) checkpoints for task $1!"
+      echo "Writing to $WRITE_FILE"
+      echo "$CHECKPOINTS" > "$WRITE_FILE"
+  fi
+)
