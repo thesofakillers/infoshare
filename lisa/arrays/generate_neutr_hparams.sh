6836e6d48037636f8753e6a4022fa4a7298b1e18
dinos <papakosk@csd.auth.gr>
2022-05-13 23:51:16 +0200

    Declutter lisa/arrays dir by organizing in subdirectories

--- lisa/arrays/generate_neutr_hparams.sh
+++ lisa/arrays/generate_neutr_hparams.sh
@@ -4,13 +4,13 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-WRITE_FILE="${1,,}_neutr_hparams.txt"
+WRITE_FILE="${1,,}/neutr_hparams.txt"
 truncate -s 0 "$WRITE_FILE" # clear the file
 
 while read -r CHECKPOINT; do
     while read -r NEUTRALIZER; do
         echo "--checkpoint ${CHECKPOINT} --neutralizer ${NEUTRALIZER}" >> "$WRITE_FILE"
-    done <<< "$(cat "${1,,}_neutralizers.txt")"
-done <<< "$(cat "all_checkpoints_${1,,}.txt")"
+    done <<< "$(cat "${1,,}/neutralizers.txt")"
+done <<< "$(cat "${1,,}/all_checkpoints.txt")"
 
 echo "Wrote $(wc -l < "$WRITE_FILE") lines to $WRITE_FILE"
--- lisa/arrays/get_checkpoints.sh
+++ lisa/arrays/get_checkpoints.sh
@@ -15,7 +15,7 @@ fi
   if [[ ${#CHECKPOINTS} -eq 0 ]]; then
       echo "No checkpoints were found for task $1!"
   else
-      WRITE_FILE="lisa/arrays/all_checkpoints_${1,,}.txt"
+      WRITE_FILE="lisa/arrays/${1,,}/all_checkpoints.txt"
       echo "Found $(echo "$CHECKPOINTS" | wc -l) checkpoints for task $1!"
       echo "Writing to $WRITE_FILE"
       echo "$CHECKPOINTS" > "$WRITE_FILE"
