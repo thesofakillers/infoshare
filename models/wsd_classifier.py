8e931308bb9cbfd49ef998ca0796e1d6e120ab77
dinos <papakosk@csd.auth.gr>
2022-11-02 21:12:09 +0100

    WIP: Log F1-score per POS tag

--- models/wsd_classifier.py
+++ models/wsd_classifier.py
@@ -1,11 +1,10 @@
-from typing import Tuple, Dict
-
-from torch.nn.utils.rnn import pad_sequence
-
 from .base_classifier import BaseClassifier
-from torch import nn, Tensor
-import torch
+from torch import nn, Tensor, LongTensor
+from torch.nn.utils.rnn import pad_sequence
 from transformers import BatchEncoding
+from typing import Tuple, Dict, List, Union
+
+import torch
 import torchmetrics.functional as TF
 
 
@@ -17,18 +16,18 @@ class WSDClassifier(BaseClassifier):
             nn.Linear(n_hidden, n_classes),
         )
 
-    def forward(self, encoded_input: BatchEncoding, salient_idxs) -> Tuple[Tensor, Tensor]:
+    def forward(self, encoded_input: BatchEncoding, salient_idxs: Tensor) -> Tuple[Tensor, Tensor]:
         embeddings = self.bert(encoded_input)
         # need to index the (padded) embeddings with salient idxs
-        embeddings = embeddings.gather(1, salient_idxs)
-        # indexing breaks padding, so pad again
+        embeddings = [emb[idx] for idx, emb in zip(salient_idxs, embeddings)]
+        # indexing breaks padding (max_sequence_len -> max_salient_len), so pad again
         embeddings = pad_sequence(embeddings, batch_first=True)
         logits = self.classifier(embeddings)
         return embeddings, logits
 
-    def process_batch(self, batch: Tuple) -> Dict[str, Tensor]:
+    def process_batch(self, batch: Tuple) -> Dict[str, Union[Tensor, List]]:
         encodings, targets, idxs, pos, lemmas = batch
-        embeddings, logits = self(encodings, idxs)  # (bs, max_seq_len, -1)
+        embeddings, logits = self(encodings, idxs)
         return {
             "embeddings": embeddings,
             "logits": logits,
@@ -41,12 +40,10 @@ class WSDClassifier(BaseClassifier):
         # log F1 score overall and per pos-tag (not per class)
         batch_logits = processed_batch["logits"]
         batch_targets = processed_batch["targets"]
-        self.log_f1(batch_logits, batch_targets, stage=stage, prefix=prefix)
+        batch_pos = processed_batch["pos"]
 
-    def log_f1(self, logits: Tensor, targets: Tensor, stage: str, prefix: str = ""):
-        batch_size = len(logits)
-
-        f1_avg = self.calc_ave_f1(logits, targets)
+        batch_size = len(batch_logits)
+        f1_avg = self.calc_avg_f1(batch_logits, batch_targets, batch_pos)
         self.log(f"{stage}_f1", f1_avg, batch_size=batch_size)
 
         if stage != "test":
@@ -54,31 +51,51 @@ class WSDClassifier(BaseClassifier):
             return
 
         # calculate & log average accuracy per-pos tag
-        acc_per_pos = self.calcu_ave_f1(logits, targets, average="none")
+        acc_per_pos = self.calc_avg_f1(batch_logits, batch_targets, batch_pos, average="none")
         for i, acc_i in enumerate(acc_per_pos):
             pos_name = self.hparams.pos_map[i]
             self.log(f"{prefix}{stage}_acc_{pos_name}", acc_i, batch_size=batch_size)
 
     @torch.no_grad()
-    def calc_ave_f1(self, logits, targets, average: str = "micro"):
+    def calc_avg_f1(
+        self,
+        batch_logits: Tensor,
+        batch_targets: List[LongTensor],
+        batch_pos: List[LongTensor],
+        average: str = "micro",
+    ):
+        n_pos_tags = len(self.hparams.pos_map)
         # Extract sequence length from number of senses to predict
-        sequence_lengths = [len(_) for _ in targets]
+        seq_lens = [len(_) for _ in batch_targets]
 
-        # TODO: figure out how to do this PER POS-TAG.
         # TODO: only consider senses from current lemma in evaluation/inference
-        # I think maybe an extra or different loop is necessary
-        # In fact we might have to fully re-write this manually
-        # calc f1 as mean over sentences
+        ## WARNING: untested code
+        batch_logits_per_pos = [
+            torch.vstack(
+                [
+                    logits[:s_len][pos == pos_id]
+                    for logits, pos, s_len in zip(batch_logits, batch_pos, seq_lens)
+                ]
+            )
+            for pos_id in range(n_pos_tags)
+        ]
+
+        batch_senses_per_pos = [
+            torch.cat([senses[pos == pos_id] for senses, pos in zip(batch_targets, batch_pos)])
+            for pos_id in range(n_pos_tags)
+        ]
+
         f1 = torch.vstack(
             [
-                TF.f1(
-                    preds=logits[i, : sequence_lengths[i], :],
-                    target=targets[i],
+                TF.f1_score(
+                    preds=batch_logits_per_pos[pos_id],
+                    target=batch_senses_per_pos[pos_id],
                     average=average,
-                    num_classes=logits.shape[-1],
+                    num_classes=batch_logits.shape[-1],
                     ignore_index=self.hparams.ignore_id,
                 )
-                for i in range(len(targets))
+                for pos_id in range(n_pos_tags)
+                # there might be an issue for missing POS in batch
             ]
         ).nanmean(dim=0)
 
