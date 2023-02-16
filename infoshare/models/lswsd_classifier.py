d0db541d6a8b182469f839bf2ccc3d64f47669e0
Giulio Starace <26286291+thesofakillers@users.noreply.github.com>
2023-02-16 16:50:05 +0100

    At inference, only consider relevant senses for given lemma

--- infoshare/models/lswsd_classifier.py
+++ infoshare/models/lswsd_classifier.py
@@ -1,7 +1,7 @@
 from typing import Tuple, Dict, Union, List, Callable, Optional
 
 import torch.nn as nn
-from torch import Tensor
+from torch import LongTensor, Tensor
 import torch
 from transformers import BatchEncoding
 from torch.nn.utils.rnn import pad_sequence
@@ -57,18 +57,31 @@ class LSWSDClassifier(BaseClassifier):
         dloader_idx: Optional[int] = None,
     ):
         batch_logits = processed_batch["logits"]
-        targets = processed_batch["targets"]
+        batch_targets = processed_batch["targets"]
+        batch_lemmas = processed_batch["lemmas"]
+
         self.log_metric(
-            batch_logits, targets, stage=stage, prefix=prefix, metric_name="acc"
+            batch_logits,
+            batch_targets,
+            batch_lemmas,
+            stage=stage,
+            prefix=prefix,
+            metric_name="acc",
         )
         self.log_metric(
-            batch_logits, targets, stage=stage, prefix=prefix, metric_name="f1"
+            batch_logits,
+            batch_targets,
+            batch_lemmas,
+            stage=stage,
+            prefix=prefix,
+            metric_name="f1",
         )
 
     def log_metric(
         self,
         logits: Tensor,
-        targets: Tensor,
+        targets: List[List[LongTensor]],
+        lemmas: List[List[str]],
         stage: str,
         prefix: str = "",
         metric_name: str = "acc",
@@ -76,7 +89,7 @@ class LSWSDClassifier(BaseClassifier):
         batch_size = len(logits)
 
         # Calculate & log average micro accuracy
-        acc_avg = self.calculate_average_metric(logits, targets, metric_name)
+        acc_avg = self.calculate_average_metric(logits, targets, lemmas, metric_name)
         self.log(f"{stage}_acc", acc_avg, batch_size=batch_size)
 
         if stage != "test":
@@ -85,7 +98,7 @@ class LSWSDClassifier(BaseClassifier):
 
         # Calculate & log average accuracy per-class
         acc_per_class = self.calculate_average_metric(
-            logits, targets, metric_name, average="none"
+            logits, targets, lemmas, metric_name, average="none"
         )
         for i, acc_i in enumerate(acc_per_class):
             class_name = self.hparams.class_map[i]
@@ -98,14 +111,21 @@ class LSWSDClassifier(BaseClassifier):
     @torch.no_grad()
     def calculate_average_metric(
         self,
-        logits: Tensor,
-        targets: Tensor,
+        batch_logits: Tensor,
+        batch_targets: List[List[LongTensor]],
+        batch_lemmas: List[List[str]],
         metric_name: str,
         average: str = "micro",
     ) -> Tensor:
-        """Calculates the mean accuracy based on the averaging method specified."""
+        """
+        Calculates the mean accuracy based on the averaging method specified.
+
+        batch_logits is B X MS X C
+        batch_targets is B X S
+        """
         # Extract sequence length from number of targets to predict
-        sequence_lengths = [len(_) for _ in targets]
+        sequence_lengths = [len(_) for _ in batch_targets]
+        all_sense_ids = set(range(self.hparams.n_classes))
 
         metric_fn: Callable
         if metric_name == "acc":
@@ -113,17 +133,30 @@ class LSWSDClassifier(BaseClassifier):
         elif metric_name == "f1":
             metric_fn = TF.f1_score
 
+        for b, (logits, targets, s_len, lemmas) in enumerate(
+            zip(batch_logits, batch_targets, sequence_lengths, batch_lemmas)
+        ):
+            # go through the lemmas in the sequence
+            for l, lemma in enumerate(lemmas):
+                possible_lemma_ids = self.hparams.lemma_to_sense_ids[lemma]
+                impossible_lemma_ids = all_sense_ids - set(possible_lemma_ids)
+                # ignore the impossible ids, by making their logits very low
+                batch_logits[b, l, list(impossible_lemma_ids)] = -torch.inf
+
+        # convert logits to preds, shape is B X MS
+        preds = torch.argmax(batch_logits, dim=-1)
+
         # Calculate metric as the mean over all sentences
         metric = torch.vstack(
             [
                 metric_fn(
-                    preds=logits[i, : sequence_lengths[i], :],
-                    target=targets[i],
+                    preds=preds[batch_el, : sequence_lengths[batch_el]],
+                    target=batch_targets[batch_el],
                     average=average,
                     num_classes=self.hparams.n_classes,
                     ignore_index=self.hparams.ignore_id,
                 )
-                for i in range(len(targets))
+                for batch_el in range(len(batch_targets))
             ]
         ).nanmean(dim=0)
 
