422ccdf0bb343275b97b9311d09fca39f3871869
Giulio Starace <26286291+thesofakillers@users.noreply.github.com>
2023-02-16 16:24:52 +0100

    [wip] Make logging more general: logs acc AND f1
    TODO: mask out senses from other lemmas when evaluating

--- infoshare/models/lswsd_classifier.py
+++ infoshare/models/lswsd_classifier.py
@@ -1,9 +1,11 @@
-from typing import Tuple, Dict, Union, List
+from typing import Tuple, Dict, Union, List, Callable, Optional
 
 import torch.nn as nn
 from torch import Tensor
+import torch
 from transformers import BatchEncoding
 from torch.nn.utils.rnn import pad_sequence
+import torchmetrics.functional as TF
 
 from infoshare.models.base_classifier import BaseClassifier
 
@@ -12,6 +14,8 @@ class LSWSDClassifier(BaseClassifier):
     """
     Same as POS Classifier but in forward we need to index the
     embeddings with salient idxs, since we no longer have a label for every word.
+
+    We also slightly modify the logging so that we also log F1 score, besides acc
     """
 
     def get_classifier_head(self, n_hidden: int, n_classes: int) -> nn.Module:
@@ -44,3 +48,83 @@ class LSWSDClassifier(BaseClassifier):
             "pos": pos,
             "lemmas": lemmas,
         }
+
+    def log_metrics(
+        self,
+        processed_batch: Dict,
+        stage: str,
+        prefix: str = "",
+        dloader_idx: Optional[int] = None,
+    ):
+        batch_logits = processed_batch["logits"]
+        targets = processed_batch["targets"]
+        self.log_metric(
+            batch_logits, targets, stage=stage, prefix=prefix, metric_name="acc"
+        )
+        self.log_metric(
+            batch_logits, targets, stage=stage, prefix=prefix, metric_name="f1"
+        )
+
+    def log_metric(
+        self,
+        logits: Tensor,
+        targets: Tensor,
+        stage: str,
+        prefix: str = "",
+        metric_name: str = "acc",
+    ):
+        batch_size = len(logits)
+
+        # Calculate & log average micro accuracy
+        acc_avg = self.calculate_average_metric(logits, targets, metric_name)
+        self.log(f"{stage}_acc", acc_avg, batch_size=batch_size)
+
+        if stage != "test":
+            # No need to log per-class accuracy for train & val
+            return
+
+        # Calculate & log average accuracy per-class
+        acc_per_class = self.calculate_average_metric(
+            logits, targets, metric_name, average="none"
+        )
+        for i, acc_i in enumerate(acc_per_class):
+            class_name = self.hparams.class_map[i]
+            self.log(
+                f"{prefix}{stage}_{metric_name}_{class_name}",
+                acc_i,
+                batch_size=batch_size,
+            )
+
+    @torch.no_grad()
+    def calculate_average_metric(
+        self,
+        logits: Tensor,
+        targets: Tensor,
+        metric_name: str,
+        average: str = "micro",
+    ) -> Tensor:
+        """Calculates the mean accuracy based on the averaging method specified."""
+        # Extract sequence length from number of targets to predict
+        sequence_lengths = [len(_) for _ in targets]
+
+        metric_fn: Callable
+        if metric_name == "acc":
+            metric_fn = TF.accuracy
+        elif metric_name == "f1":
+            metric_fn = TF.f1_score
+
+        # Calculate metric as the mean over all sentences
+        metric = torch.vstack(
+            [
+                metric_fn(
+                    preds=logits[i, : sequence_lengths[i], :],
+                    target=targets[i],
+                    average=average,
+                    num_classes=self.hparams.n_classes,
+                    ignore_index=self.hparams.ignore_id,
+                )
+                for i in range(len(targets))
+            ]
+        ).nanmean(dim=0)
+
+        return metric
